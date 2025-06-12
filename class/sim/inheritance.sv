b1b064f423e19d8d471a1e287d518ceb15380131
Dvir Hershkovits <dvirhersh@gmail.com>
2025-06-12 17:33:55 +0300

    this and super.
    this  - handle to *current* class
    super - handle to *parent*  class
    
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- class/sim/inheritance.sv
+++ class/sim/inheritance.sv
@@ -1,32 +1,54 @@
+`timescale 1ns/1ps
+
 class parent;
-    int p1;
+    bit b1;
 
-    function new(input int pp1);
-        p1 = pp1;
-    endfunction
-
-endclass
-
-class child extends parent;
-    int c1;
-
-    function new(input int pp1, pc1);
-        super.new(pp1);
-        c1 = pc1;        
+    function new(input int p1);
+        b1 = p1;
     endfunction
 
     function void print();
-        $display("child: p1 = %0d, c1 = %0d", p1, c1);
+        $display("parent: b1 = %0d", b1);
+    endfunction
+endclass
+
+class child extends parent;
+    bit [1:0] p1;
+
+    function new(input bit [1:0] p1);
+        super.new(p1);
+        this.p1 = p1;
+    endfunction
+
+    function void print();
+        super.print();  // print b1 from parent
+        $display("child : p1 = %0d", p1);
+    endfunction
+endclass
+
+class grandchild extends child;
+    function new(input bit p1);
+        super.new(p1);
+    endfunction
+
+    function void print();
+        super.print();
+        $display("grandchild: no extra fields");
     endfunction
 endclass
 
 module inheritance;
-    child child1;
+    parent     parent1;
+    child      child2;
+    grandchild grandchild3;
 
     initial begin
-        child1 = new(.pp1(4), .pc1(2));
-        child1.print();
-        child1.p1 = 9;
-        child1.print();
+        parent1     = new(1'b1);
+        child2      = new(2'b10);
+        grandchild3 = new(2'b11);
+
+        parent1.print();
+        child2.print();
+        grandchild3.print();
     end
 endmodule
