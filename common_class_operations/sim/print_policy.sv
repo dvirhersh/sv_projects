7ccf705be52e25ac6fa2bb92043eda538ef6ae29
Dvir Hershkovits <dvirhersh@gmail.com>
2025-06-15 16:42:47 +0300

    Polymorphism and Encapsulation
    - Each class is responsible for printing it's own properties
    
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- common_class_operations/sim/print_policy.sv
+++ common_class_operations/sim/print_policy.sv
@@ -10,17 +10,43 @@ class base;
             DEC : $display("a_byte: 0d'%0d", a_byte);
         endcase
     endfunction
+
+    virtual function void print_p();
+        $display("a_byte = %0h", a_byte);
+    endfunction
 endclass
+
+class child extends base;
+    bit[3:0] nibble;
+    virtual function void print_p();
+        super.print_p();
+        $display("nibble = %0h", nibble);
+    endfunction
+endclass 
 
 module print_policy;
 
-    base bc = new();
+    base  bc = new();
+    child cc = new();
 
     initial begin
         bc.a_byte = 14;
+        cc.a_byte = 21;
+        cc.nibble = 9;
         bc.print();
         bc.print(HEX);
         bc.print(BIN);
         bc.print(DEC);
+
+        // ----
+        $display("Polymorphism and Encapsulation");
+        $display("1");
+        bc.print_p();
+        $display("2");
+        cc.print_p();
+        bc = cc;
+        $display("3");
+        bc.print_p();
+
     end
 endmodule
