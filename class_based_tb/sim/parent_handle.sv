41a829e744fa7679411f72e9653b1825ea04f0da
Dvir Hershkovits <dvirhersh@gmail.com>
2025-06-16 16:51:19 +0300

    Creating a Pathname with Parent Handles
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- class_based_tb/sim/parent_handle.sv
+++ class_based_tb/sim/parent_handle.sv
@@ -1,4 +1,5 @@
 virtual class base;
+
     string inst;
     base   parent;
 
@@ -6,6 +7,16 @@ virtual class base;
         inst   = name;
         parent = up;
     endfunction
+
+    function string pathname();
+        base ptr = this;
+        pathname = inst;
+        while (ptr.parent != null) begin
+            ptr = ptr.parent;
+            pathname = {ptr.inst, ".", pathname};
+        end
+    endfunction : pathname
+
 endclass : base
 
 class sequencer extends base;
@@ -13,6 +24,10 @@ class sequencer extends base;
     function new(input string name, base up);
         super.new(name, up);
     endfunction
+
+    function void print();
+        $display("@ %s", this.pathname());
+    endfunction : print
 
 endclass : sequencer
 
@@ -28,15 +43,18 @@ class pds_vc extends base;
 endclass : pds_vc
 
 module parent_handle;
+
     pds_vc pds1 = new("pds1", null);
     base   ptr;
 
     initial begin
         ptr = pds1.seqr;
-        if (ptr != null) $display("ptr.inst     = %s", ptr.inst);
 
-        ptr = ptr.parent;
-        if (ptr != null) $display("ptr.parent.inst = %s", ptr.inst);
+        if (ptr != null) $display("ptr.inst = %s", ptr.inst);
+
+        if (ptr.parent != null) $display("ptr.parent.inst = %s", ptr.parent.inst);
         else $display("ptr.parent = null");
+
+        pds1.seqr.print();  // Expected output: @ pds1.seqr
     end
 endmodule
