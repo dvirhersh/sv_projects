7ce2aacb55c115aa037bebe08818de09e2352c39
Dvir Hershkovits <dvirhersh@gmail.com>
2025-06-16 13:06:28 +0300

    Instance Names
    - The solution to distinguish multiple instances
    
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- common_class_operations/sim/aggregate_class.sv
+++ common_class_operations/sim/aggregate_class.sv
@@ -1,8 +1,23 @@
+`timescale 1ns / 1ps
+
 class baseframe_1;
     bit b1;
+    protected string inst;
+
+    function new(input string pst = "");
+        inst = pst;
+    endfunction
+
+    function string getname();
+        return inst;
+    endfunction
+
+    function void setname(input string pst);
+        inst = pst;
+    endfunction
 
     function void print();
-        $display("b1 = %b", b1);
+        $display(inst, ".b1 = %b", b1);
     endfunction
 endclass
 
@@ -12,8 +27,8 @@ class multiframe_1;
     baseframe_1 frame2;
 
     function new();
-        frame1 = new();
-        frame2 = new();
+        frame1 = new("frame1");
+        frame2 = new("frame2");
     endfunction
 
     function void print();
@@ -24,11 +39,24 @@ class multiframe_1;
 endclass
 
 module aggregate_class;
+    multiframe_1 one = new();
+    baseframe_1  bc1 = new("bc1");
+    baseframe_1  bc2;
+
     initial begin
-        multiframe_1 one = new();
-        one.count = 42;  // example value
+        one.count = 42;
         one.frame1.b1 = 1;
         one.frame2.b1 = 0;
         one.print();
+
+        // =========== Object copying test ===========
+        $display("bc1 name = %s", bc1.getname());
+
+        // Clone bc1 to bc2 (use copy constructor if needed)
+        bc2 = new(bc1.getname());
+        $display("bc2 (copy of bc1) name = %s", bc2.getname());
+
+        bc2.setname("bc2");
+        $display("bc2 (renamed) name = %s", bc2.getname());
     end
 endmodule
