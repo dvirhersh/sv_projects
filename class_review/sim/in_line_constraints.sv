530892dd3e80209cce9092af2e64d0083c74d2a1
Dvir Hershkovits <dvirhersh@gmail.com>
2025-06-24 21:39:23 +0300

    In-Line Constraints
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- class_review/sim/in_line_constraints.sv
+++ class_review/sim/in_line_constraints.sv
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 
-class RandFrame;
+class RandFrame2;
 
          local bit [3:0] addr;
-    rand local bit [3:0] len;
-         bit [7:0] data_arr[];
+    rand       bit [3:0] len;
+               bit [7:0] data_arr[];
 
     // Constructor
     function new(input bit [3:0] paddr);
@@ -12,41 +12,41 @@ class RandFrame;
     endfunction
 
     function void post_randomize();
-        $display("Dvir debug: post_randomize called");
         data_rand();
     endfunction
 
     function void data_rand();
         data_arr = new[len];
-        foreach (data_arr[i])
+        foreach (data_arr[i]) 
             data_arr[i] = $urandom;
     endfunction
 
-    // Prin the frame content
     function void print();
-        $display("=== RandFrame Info ===");
         $display("addr = 0x%0h, len = %0d, data_arr.size() = %0d", addr, len, data_arr.size());
         $write("data_arr = ");
         foreach (data_arr[i]) 
             $write("0x%02x ", data_arr[i]);
-        $display("\n=======================");
+        $display();  // newline
     endfunction
 
 endclass
 
-module RandFrameModule;
-    RandFrame frame1 = new(.paddr(4'd5));
+module InLineConstraintsModule;
+    RandFrame2 frame1 = new(4'd5);
     int ok;
 
-    initial begin
+  initial begin
+        ok = frame1.randomize() with { len > 0; len <= 7; };
+        if (!ok) 
+            $fatal("Randomization failed (range 1–7)");
         frame1.print();
-        $display("Dvir debug: randomizing frame1...");
-        ok = frame1.randomize();
-        if (!ok)
-            $fatal(1, "Randomization failed for frame1");
 
+        ok = frame1.randomize() with {len dist { [1 :  7] := 1, [8 : 15] := 2}; };
+        if (!ok) 
+            $fatal("Randomization failed (dist)");
         frame1.print();
 
         $stop;
     end
+
 endmodule
