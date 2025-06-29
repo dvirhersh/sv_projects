6179d4e10ea42cc52ed03b82108d0c7542fb026c
Dvir Hershkovits <dvirhersh@gmail.com>
2025-06-29 17:22:18 +0300

    Printing Method Example
    Print Policy
    
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- common_class_operations/sim/package.sv
+++ common_class_operations/sim/package.sv
@@ -1,4 +1,7 @@
 package class_operations;
+
+    typedef enum {HEX, BIN, DEC} format_e;
+
     class baseframe;
         bit [3:0] addr;
 
@@ -24,4 +27,33 @@ package class_operations;
             f1 = new(f1adr);
         endfunction
     endclass
+
+    class base;
+        bit [7:0] a_byte;
+
+        function void print(format_e format = HEX);
+        begin
+            case (format)
+                HEX: $display("a_byte: 0x%0h", a_byte);
+                BIN: $display("a_byte: 0b%0b", a_byte);
+                DEC: $display("a_byte: %0d"  , a_byte);
+                default: $display("a_byte: unknown format");
+            endcase
+        end
+        endfunction
+
+        virtual function void print_p();
+            $display("a_byte = %0h", a_byte);
+        endfunction
+    endclass
+
+    class child extends base;
+        bit [3:0] nibble;
+
+        virtual function void print_p();
+            super.print_p();
+            $display("nibble = %0h", nibble);
+        endfunction
+    endclass
+
 endpackage
--- common_class_operations/sim/print_policy.sv
+++ common_class_operations/sim/print_policy.sv
@@ -1,30 +1,6 @@
-typedef enum {HEX, BIN, DEC} format_e;
+import class_operations::*;
 
-class base;
-    bit [7:0] a_byte;
-
-    function void print(format_e format = HEX);
-        case (format)
-            HEX : $display("a_byte: 0x'%0h", a_byte);
-            BIN : $display("a_byte: 0b'%0b", a_byte);
-            DEC : $display("a_byte: 0d'%0d", a_byte);
-        endcase
-    endfunction
-
-    virtual function void print_p();
-        $display("a_byte = %0h", a_byte);
-    endfunction
-endclass
-
-class child extends base;
-    bit[3:0] nibble;
-    virtual function void print_p();
-        super.print_p();
-        $display("nibble = %0h", nibble);
-    endfunction
-endclass 
-
-module print_policy;
+module PrintPolicy;
 
     base  bc = new();
     child cc = new();
